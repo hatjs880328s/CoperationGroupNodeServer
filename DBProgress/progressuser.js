fa305a9bfbfec31af9ea98e98bc8d2eb82839879
Noah_Shan <shanwzh@inspur.com>
2019-11-29 14:31:48 +0800

    版本:
    测试post
    
    更新内容：
    
    Author: Noah_shan

--- DBProgress/progressuser.js
+++ DBProgress/progressuser.js
@@ -1,7 +1,7 @@
 /*
  * @Author: noah shan
  * @Date: 2019-11-29 10:01:42
- * @LastEditTime: 2019-11-29 10:33:28
+ * @LastEditTime: 2019-11-29 13:40:37
  * @LastEditors: Please set LastEditors
  * @Description: 处理user表所有操作
  * @FilePath: /CoperationGroupNodeServer/DBProgress/progressuser.js
@@ -33,7 +33,8 @@ function getUserWith(connection, uid, any) {
 
 /// 添加一个用户，传入一个user json obj
 function addUserWith(usermodel, any) {
-    var sql = 'insert into users values (\'\', \'\', \'\', \'\')';
+    var sql = 'insert into users values (\'${usermodel[""]}\', \'\', \'\', \'\')';
+    console.log('halo.moto, ${sql}');
 }
 
  module.exports = {getAlluser, getUserWith, addUserWith};
--- Main/main.js
+++ Main/main.js
@@ -1,41 +1,61 @@
 /*
  * @Author: noah shan
  * @Date: 2019-11-28 16:06:10
- * @LastEditTime: 2019-11-29 10:22:12
+ * @LastEditTime: 2019-11-29 14:29:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /CoperationGroupNode/ConnectDB.js
  */
 
- /// config instance
- const appconfig = require('../Config/config');
+/// config instance
+const appconfig = require('../Config/config');
 
- /// mysql instance
- const mysql = require('../UTI/ConnectDB');
+/// mysql instance
+const mysql = require('../UTI/ConnectDB');
 
- /// api instance
- const app = require('../UTI/ConnectAPI');
+/// api instance
+const app = require('../UTI/ConnectAPI');
 
- /// db config 
- var dbinstance = mysql.mysqlconnect();
+/// db config 
+var dbinstance = mysql.mysqlconnect();
 
- /// api config
- app.config();
+/// api config
+app.config();
 
- // users db progress ins
- const userdbIns = require('../DBProgress/progressuser');
-  
- /// get all user infos
- app.apiconfig.get('/users', function(err, res) {
+// users db progress ins
+const userdbIns = require('../DBProgress/progressuser');
+
+/// get all user infos
+app.apiconfig.get('/users', function (err, res) {
     console.log('have someone request users');
-    userdbIns.getAlluser(dbinstance, function(result) {
+    userdbIns.getAlluser(dbinstance, function (result) {
         res.json(result);
     });
- })   
- 
- /// 开启监听
- var server = app.apiconfig.listen(8081, appconfig.hostapi, function () {
-     var host = server.address().address;
-     var port = server.address().port;
-     console.log("监听地址为 http://%s:%s", host, port);
- })
+})
+
+/// post req
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.apiconfig.post('/post', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    console.log(req.body.name);
+    var json = JSON.stringify({
+        msg: "成功",
+        result: [{
+            "name": "菜鸟教程",
+            "site": "http://www.runoob.com"
+        }, {
+            "name": "新手教程",
+            "site": "csdnnnn"
+        }],
+        status: 1
+    });
+    res.end(json);
+});
+
+/// 开启监听
+var server = app.apiconfig.listen(8081, appconfig.hostapi, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("监听地址为 http://%s:%s", host, port);
+})

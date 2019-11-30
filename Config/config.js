a8363ddd9955ba5456ff1b9f7b5aa0827f546580
Noah_Shan <shanwzh@inspur.com>
2019-11-30 15:28:09 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- Config/config.js
+++ Config/config.js
@@ -1,15 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2019-11-28 19:28:16
- * @LastEditTime: 2019-11-28 19:28:49
- * @LastEditors: your name
+ * @LastEditTime: 2019-11-30 15:28:03
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /CoperationGroupNode/config.js
  */
- /// 配置 api
+ /// 配置 api [remote server is: 172.17.0.15]
 const hostapi = '127.0.0.1';
 
-/// 配置mysql - pwd
+/// 配置mysql - pwd [remote server is: 123456]
 const mysqlpwd = '123456789';
 
 module.exports = {hostapi, mysqlpwd};

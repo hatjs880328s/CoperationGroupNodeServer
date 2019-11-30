01133e205557603c3ac3cc1d9005da6700fba2e2
Noah_Shan <shanwzh@inspur.com>
2019-11-30 21:24:15 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

---
+++ BLL/normalbll.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-30 21:15:32
+ * @LastEditTime: 2019-11-30 21:20:30
+ * @LastEditors: Please set LastEditors
+ * @Description: 处理业务代码
+ * @FilePath: /CoperationGroupNodeServer/BLL/normalbll.js
+ */
+
+
+ // 处理普通文件
+ 
+ // 处理文件夹
+ // 处理协同组文件
+ // 处理协同组文件夹
+ // 处理个人信息

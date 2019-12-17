71bb9276caffeace361703a1eabed0a78a5bdad2
Noah_Shan <shanwzh@inspur.com>
2019-12-17 11:03:06 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- UTI/ConnectDB.js
+++ UTI/ConnectDB.js
@@ -1,7 +1,7 @@
 /*
  * @Author: noah shan
  * @Date: 2019-11-28 16:06:10
- * @LastEditTime: 2019-11-29 09:52:21
+ * @LastEditTime: 2019-12-17 11:01:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /CoperationGroupNode/ConnectDB.js
@@ -19,6 +19,16 @@ function mysqlconnect() {
         database: 'CoperationGroupDB'
     });
     connection.connect();
+
+    connection.on('error', function(err) {
+        console.log(`mysql connection error: ${err}`);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('mysql 进行重连操作');
+            connection.connect();
+        } else {
+            throw err;
+        }
+    });
     return connection;
 }
 

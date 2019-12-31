8afaa1623b8113087d90b61dccf7f769e2720872
Noah_Shan <shanwzh@inspur.com>
2019-12-31 16:52:05 +0800

    版本:
    更新内容：
    
    Author: Noah_shan

--- DBProgress/progressTSSC.js
+++ DBProgress/progressTSSC.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-12-31 09:17:22
- * @LastEditTime : 2019-12-31 16:09:24
+ * @LastEditTime : 2019-12-31 16:29:52
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /CoperationGroupNodeServer/DBProgress/progressTSSC.js
@@ -10,9 +10,10 @@
 /// 唐诗宋词 db progress
 
 /// 将一个文件中的数据进行遍历，然后插入
-async function addTSSCWith(connection, filemodel, any) {
+async function addTSSCWith(connection, bodyinfo, any) {
     var dbuti = require('../ThirdLib/cgdbuti');
     var sqls = [];
+    var filemodel = bodyinfo["infos"];
     for (var i = 0 ; i < filemodel.length ; i++) {
         var delSQL = `delete from TSSC where id = '${filemodel[i]['id']}'`;
         var eachSQL = `insert into TSSC values ('${filemodel[i]['id']}', '${filemodel[i]['title']}', '${filemodel[i]['author']}', '${filemodel[i]['paragraphs']}');`;
